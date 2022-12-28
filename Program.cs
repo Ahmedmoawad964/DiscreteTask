61e0b1756d028d04e7671fe52a8b1954cf9928a3
Ahmedmoawad964 <121581982+Ahmedmoawad964@users.noreply.github.com>
2022-12-29 00:07:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discrete_project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the first number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number ");
+            int second = int.Parse(Console.ReadLine());
+             Console.WriteLine("----------------------");
+            for (int i = first; i <=second ; i++) 
+            {
+                if (i == 0 || i == 1)
+                    continue;
+                int c = 0;
+               
+                for (int j = 2; j <= i /2; j++) 
+                {
+
+                    if (i % j == 0)
+                    {
+                        c++; 
+                        break;
+                    }
+                }
+                if (c == 0 )
+                    Console.WriteLine(i); 
+
+              
+
+            }
+                Console.WriteLine("----------------------\nend");
+                   
+
+
+        }
+    }
+}
